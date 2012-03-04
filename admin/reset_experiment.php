8087242fecb1ce68fbf921f3c0badbb5d9372917
Jonathan Roy <jroy@optimumweb.ca>
2012-03-04 02:17:38 -0500

    Added reset_experiment feature

---
+++ admin/reset_experiment.php
@@ -0,0 +1,28 @@
+<?php
+$tab = 'experiments';
+
+$id = (int)$_GET['id'];
+$var = $wpdb->get_row($wpdb->prepare("SELECT * FROM ".$wpdb->prefix."abtest_experiments WHERE id=%d", $id));
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  // Delete the variation
+  $wpdb->query($wpdb->prepare("DELETE FROM ".$wpdb->prefix."abtest_goal_hits WHERE goal_id IN (SELECT id FROM ".$wpdb->prefix."abtest_goals WHERE experiment_id=%d)", $id));
+  $wpdb->query($wpdb->prepare("DELETE FROM ".$wpdb->prefix."abtest_variation_views WHERE variation_id IN (SELECT id FROM ".$wpdb->prefix."abtest_variations WHERE experiment_id=%d)", $id));
+  
+  redirect_to('?page=abtest&action=reset_experiment&id=' . $var->experiment_id);
+}
+?>
+
+<?php include 'tabs.php' ?>
+<div class="wrap">
+  <h3>Reset experiment</h3>
+  <form method="post">
+    <p>
+      Are you sure you want to reset the experiment <em><?php echo htmlspecialchars($var->name) ?></em>? This can't be undone.
+    </p>
+    <p>
+      <input class="button-primary" type="submit" name="Save" value="Reset variation" id="submitbutton" />
+      or <a href="?page=abtest&amp;action=show_experiment&amp;id=<?php echo $var->experiment_id ?>">Cancel</a>
+    </p>
+  </form>
+</div>
